cf54f45cb0428ce39d4ca4453341d58227549ab2
Harold Pineda <hrpinedar91@gmail.com>
2022-01-12 18:58:24 -0500

    refactory arrow function

--- Figuras/figuras.js
+++ Figuras/figuras.js
@@ -2,17 +2,20 @@
 
 console.group("Triangulo")
 
-const base = 4
-const side1 = 6
-const side2 = 6
-const heigth = 5.5
+// const base = 4
+// const side1 = 6
+// const side2 = 6
+// const heigth = 5.5
 
-const perimeterT = base + side1 + side2
-const areaTriangulo = (base * heigth) / 2
-console.log(`Los lados del triangulo miden: ${base}cm, ${side1}cm y ${side2}cm`)
-console.log(`La altura del triangulo es de ${heigth}`)
-console.log(`El perimetro del triangulo es: ${perimeterT}`)
-console.log(`El area del triangulo es: ${areaTriangulo} cm2`)
+// const perimeterT = base + side1 + side2
+// const areaTriangulo = (base * heigth) / 2
+const perimeterT = (base, side1, side2) => base + side1 + side2
+const areaTriangulo = (base, heigth) => (base * heigth) / 2
+
+// console.log(`Los lados del triangulo miden: ${base}cm, ${side1}cm y ${side2}cm`)
+// console.log(`La altura del triangulo es de ${heigth}`)
+// console.log(`El perimetro del triangulo es: ${perimeterT}`)
+// console.log(`El area del triangulo es: ${areaTriangulo} cm2`)
 
 console.groupEnd()
 
@@ -20,14 +23,16 @@ console.groupEnd()
 
 console.group("Cuadrado")
 
-const sideSquare = 5
-console.log(`Los lados del cuadrado miden ${sideSquare}cm`)
+// const sideSquare = 5
+// console.log(`Los lados del cuadrado miden ${sideSquare}cm`)
 
-const perimeterSquare = sideSquare * 4
-console.log(`El perimetro del cuadrado es ${perimeterSquare}cm`)
+// const perimeterSquare = sideSquare * 4
+// console.log(`El perimetro del cuadrado es ${perimeterSquare}cm`)
+const perimeterSquare = (sideSquare) => sideSquare * 4 
 
-const areaSquare = sideSquare * sideSquare
-console.log(`El area del cuadrado es: ${areaSquare}cm2 `)
+// const areaSquare = sideSquare * sideSquare
+// console.log(`El area del cuadrado es: ${areaSquare}cm2 `)
+const areaSquare = (sideSquare) => sideSquare * sideSquare
 
 console.groupEnd()
 
@@ -35,15 +40,19 @@ console.groupEnd()
 
 console.group("Circulo")
 
-const ratio = 10
-const diameter = ratio * 2
-const PI = Math.PI
+// const ratio = 10
+// const diameter = ratio * 2
+const diameter = (ratio) => ratio * 2
+// const PI = Math.PI
 
-const perimeterCircule = diameter * PI
-const areaCircule = PI * (ratio * ratio)
+// const perimeterCircule = diameter * PI
+const perimeterCircule = (diameter) => diameter * Math.PI
 
-console.log(`El perimetro del circulo es. ${perimeterCircule}cm`)
-console.log(`El area del circulo es. ${areaCircule}cm`)
+// const areaCircule = PI * (ratio * ratio)
+const areaCircule = (ratio) => { Math.PI * (ratio * ratio) }
+
+// console.log(`El perimetro del circulo es. ${perimeterCircule}cm`)
+// console.log(`El area del circulo es. ${areaCircule}cm`)
 
 
 console.groupEnd
